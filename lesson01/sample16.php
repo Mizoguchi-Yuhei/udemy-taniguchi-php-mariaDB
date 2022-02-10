556fd5ea8d46ed6d5e0a5c60383bdf84fc9b8567
Mizoguchi-Yuhei <mizo.cb.fl@gmail.com>
2022-02-10 14:54:49 +0900

    2-24

--- lesson01/sample16.php
+++ lesson01/sample16.php
@@ -1,3 +1,9 @@
 <?php
-//
-readfile('data/news.txt');
+$news = file_get_contents('data/news.txt');
+echo $news;
+
+// ファイルの追記
+$news = $news . "<br>追加のニュースです";
+$success = file_put_contents('data/news.txt', $news);
+
+// readfile('data/news.txt');
