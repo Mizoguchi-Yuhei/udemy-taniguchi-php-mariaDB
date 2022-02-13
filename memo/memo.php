b82301c526e67dc6821ac7557bc1737fdf305a00
Mizoguchi-Yuhei <mizo.cb.fl@gmail.com>
2022-02-13 15:50:04 +0900

    セクション4 80

--- memo/memo.php
+++ memo/memo.php
@@ -18,16 +18,29 @@
     $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
     if (!$id) {
         echo '表示するメモを指定してください';
+        exit();
     } else {
         $stmt->bind_param('i', $id);
         $stmt->execute();
     }
 
     $stmt->bind_result($id, $memo, $created);
-    $stmt->fetch();
+    $result = $stmt->fetch();
+    if (!$result) {
+        echo '指定されたメモは見つかりませんでした';
+        exit();
+    }
     ?>
 
-    <div><?php echo htmlspecialchars($memo); ?></div>
+    <div>
+        <pre><?php echo htmlspecialchars($memo); ?></pre>
+    </div>
+
+    <p>
+        <a href="update.php?id=<?php echo $id; ?>">編集する</a>
+        <a href="delete.php?id=<?php echo $id; ?>">削除する</a>
+        <a href="/udemy-taniguchi-php+MariaDB/memo/">一覧へ</a>
+    </p>
 </body>
 
 </html>
